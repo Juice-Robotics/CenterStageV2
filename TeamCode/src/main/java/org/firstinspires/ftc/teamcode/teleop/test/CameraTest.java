01f8af8eec724e8d42f524a8000e76ff7281d743
Hunter T <awesomehtr1@gmail.com>
2024-02-23 17:03:33 -0800

    cam works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/CameraTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.teleop.test;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@TeleOp(name = "CameraTest")
+@Config
+public class CameraTest extends LinearOpMode {
+
+    OpenCvCamera camera;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // Usually this is where you'll want to start streaming from the camera
+                camera.startStreaming(1280, 800, OpenCvCameraRotation.UPRIGHT);
+//                FtcDashboard.getInstance().startCameraStream(camera, 0);
+            }
+            @Override
+            public void onError(int errorCode) {
+                // This will be called if the camera could not be opened
+//                Robot.addErrorMessage("camera no work");
+            }
+        });
+
+        while (!isStarted() && !isStopRequested()) { sleep(20); }
+        while (opModeIsActive()) { sleep(20);   }
+    }
+
+}
